4839779e5bbef31922e24184f13b7c9d4b56ba12
Reshma John <reshma.chackola@gmail.com>
2025-05-02 12:05:31 +0200

    Changes to Hugging face api call

--- src/ai.js
+++ src/ai.js
@@ -8,7 +8,6 @@ You are an assistant that receives a list of ingredients that a user has and sug
 // for HF_ACCESS_TOKEN
 
 const hf = new HfInference(import.meta.env.VITE_HF_ACCESS_TOKEN);
-console.log("Hugging Face Token:", import.meta.env.VITE_HF_ACCESS_TOKEN);
 
 export async function getRecipeFromMistral(ingredientsArr) {
   const ingredientsString = ingredientsArr.join(", ");
