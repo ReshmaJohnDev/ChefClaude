71aa6f5297be4aeee32aa7f183806c70a2a36f2f
Reshma John <reshma.chackola@gmail.com>
2025-05-02 08:12:44 +0200

    Invoking API

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+import Body from "./Body";
+
+export default function App() {
+  console.log("test");
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+}
